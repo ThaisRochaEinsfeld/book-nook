9487b8320fa4d76ce5f1c43f63958b3cbb8ff0d7
thaiseinsfelddarocha <t.da-rocha@cf-partners>
2025-05-16 07:17:16 -0300

    add images

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -3,6 +3,14 @@ import Layout from "../components/Layout";
 import SearchInput from "../components/SearchInput";
 import BookList from "../components/BookList"; 
 import dunaImage from '../assets/duna.png';
+import neuromancer from '../assets/neuromancer.png';
+import fundacao from '../assets/fundacao.png';
+import robo from '../assets/robo.png';
+import admiravel from '../assets/admiravel.png';
+import infancia from '../assets/infancia.png';
+import solaris from '../assets/solaris.png';
+import escuridao from '../assets/escuridao.png';
+
 
 const books = [
   {
@@ -17,7 +25,7 @@ const books = [
     id: "2",
     title: "Fundação",
     author: "Isaac Asimov",
-    image: dunaImage,
+    image: fundacao,
     description: "A saga galáctica de Asimov.",
     status: "Não lido" as "Não lido", 
   },
@@ -25,7 +33,7 @@ const books = [
     id: "3",
     title: "Neuromancer",
     author: "William Gibson",
-    image: dunaImage,
+    image: neuromancer,
     description: "Cyberpunk puro.",
     status: "Em progresso" as "Em progresso",  
   },
@@ -33,40 +41,40 @@ const books = [
     id: "4",
     title: "Eu, Robô",
     author: "Isaac Asimov",
-    image: dunaImage,
+    image: robo,
     description: "Coleção de contos sobre robôs.",
     status: "Em progresso" as "Em progresso",  
   },
   {
     id: "5",
-    title: "Eu, Robô",
-    author: "Isaac Asimov",
-    image: dunaImage,
-    description: "Coleção de contos sobre robôs.",
+    title: "Admirável Mundo Novo",
+    author: "Aldous Huxley",
+    image: admiravel,
+    description: "Uma sociedade futurista moldada por engenharia genética e condicionamento psicológico.",
     status: "Em progresso" as "Em progresso",  
   },
   {
     id: "6",
-    title: "Eu, Robô",
-    author: "Isaac Asimov",
-    image: dunaImage,
-    description: "Coleção de contos sobre robôs.",
+    title: "O Fim da Infância",
+    author: "Arthur C. Clarke",
+    image: infancia,
+    description: "A chegada de alienígenas benevolentes transforma a civilização humana para sempre.",
     status: "Em progresso" as "Em progresso",  
   },
   {
     id: "7",
-    title: "Eu, Robô",
-    author: "Isaac Asimov",
-    image: dunaImage,
-    description: "Coleção de contos sobre robôs.",
+    title: "Solaris",
+    author: "Stanisław Lem",
+    image: solaris,
+    description: "Cientistas tentam compreender o misterioso oceano pensante do planeta Solaris.",
     status: "Em progresso" as "Em progresso",  
   },
   {
     id: "8",
-    title: "Eu, Robô",
-    author: "Isaac Asimov",
-    image: dunaImage,
-    description: "Coleção de contos sobre robôs.",
+    title: "A Mão Esquerda da Escuridão",
+    author: "Ursula K. Le Guin",
+    image: escuridao,
+    description: "Explora temas de gênero e cultura em um planeta onde os habitantes não possuem sexo fixo.",
     status: "Em progresso" as "Em progresso",  
   },
 ];
