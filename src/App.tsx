d396e5e76e006fa1d01a7388b8012d564708f47a
thaiseinsfelddarocha <t.da-rocha@cf-partners>
2025-05-20 18:42:28 -0300

    remove unnecessary code

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,7 @@
 import React from 'react';
 import './App.css';
 import Search from './pages/Search';
-import dunaImage from './assets/duna.png';
-import neuromancer from './assets/neuromancer.png';
-import fundacao from './assets/fundacao.png';
-import robo from './assets/robo.png';
-import admiravel from './assets/admiravel.png';
-import infancia from './assets/infancia.png';
-import solaris from './assets/solaris.png';
-import escuridao from './assets/escuridao.png';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import BookList from "./components/BookList";
 import BookDetail from "./pages/BookDetails"; 
 import Profile from './pages/Profile';
 import Shelf from './pages/Shelf';
